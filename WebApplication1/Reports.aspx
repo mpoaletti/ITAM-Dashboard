88d400aef51e5649cddd370a4a793a1cff78e35a
efurzlan <efurzlan@uwsuper.edu>
2024-01-30 08:42:09 -0600

    Added Report Page (renamed About page to be reports).  Added funtionality to generate report to excel file.  Currently reports all data from the database table.  To be added: Options/functionality to restrict columns reported back and filter criteria.

--- WebApplication1/Reports.aspx
+++ WebApplication1/Reports.aspx
@@ -3,7 +3,9 @@
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
     <main aria-labelledby="title">
         <h2 id="title"><%: Title %></h2>
-        <h3>&nbsp;</h3>
+        <h3>Report on all items:&nbsp;
+			<asp:Button ID="AllItemsReport" runat="server" OnClick="AllItemsReport_Click" Text="Generate Report" />
+	</h3>
         <p>&nbsp;</p>
     </main>
 </asp:Content>
--- WebApplication1/Reports.aspx.cs
+++ WebApplication1/Reports.aspx.cs
@@ -1,17 +1,93 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
+//using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using Excel = Microsoft.Office.Interop.Excel;
+
 namespace WebApplication1
 {
-    public partial class Reports : Page
+    public partial class Reports : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
-    }
+
+		protected void AllItemsReport_Click(object sender, EventArgs e)
+		{
+            BoilerPlateDraft.Code_Repo.DatabaseConnection dbconn = new BoilerPlateDraft.Code_Repo.DatabaseConnection();
+            System.Data.DataTable dt;
+
+            dt = dbconn.SearchInventoryItem("","","");
+
+            //dt.Columns.Count
+
+            Excel.Application objExcel = new Excel.Application();
+            Excel.Workbook workBook;
+            Excel.Worksheet workSheet;
+
+            if(File.Exists("ITAM Report - All")) {
+                workBook = objExcel.Workbooks.Add("ITAM Report - All");
+                workSheet = workBook.Sheets["ITAM Report All"];
+            }
+            else {
+                workBook = objExcel.Workbooks.Add();
+                workSheet = workBook.Worksheets.Add();
+                workSheet.Name = "ITAM Report All";
+            }
+
+            try{
+
+                int excelRowNum = 1;
+                
+                //get column headers
+                for(int i = 0; i < dt.Columns.Count; i++) {
+                    workSheet.Cells[excelRowNum, i + 1].Value = dt.Columns[i].ColumnName;
+                    workSheet.Cells[excelRowNum, i + 1].Interior.Color = System.Drawing.Color.Gray;
+                    workSheet.Columns[i + 1].AutoFit();
+                    //Excel.Range columnHeader = workSheet.Range[workSheet.Cells[excelRowNum, i + 1]];
+                    //columnHeader.Interior.Color = System.Drawing.Color.Gray;
+                    //Excel.Range rng = workSheet.Columns[i + 1];
+                    //rng.AutoFit();
+                }
+
+                //Set excelRowNum to 1 as Excel is 1 based, not 0 based.
+                excelRowNum = 2;
+
+                int rowNum = 0;
+                foreach(DataRow row in dt.Rows) {
+                    for(int i = 0; i < dt.Columns.Count; i++) {
+                        workSheet.Cells[excelRowNum,i+1].Value = row[i].ToString();
+                    }
+                    rowNum++;
+                    excelRowNum++;
+                }
+
+                for (int i = 0; i < dt.Columns.Count; i++)
+                {
+                    workSheet.Columns[i + 1].AutoFit();
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            try{
+                workBook.SaveAs("ITAM Report - All");
+                workBook.Close();
+                objExcel.Quit();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+	}
 }
